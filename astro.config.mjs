90520991cdbc90349baa2195ff5471e054b4e269
Anthony Bullard <anthony.bullard@gmail.com>
2023-07-08 08:24:02 -0500

    Breaks with node adapter as well

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 
-import netlify from "@astrojs/netlify/functions";
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify(),
+  adapter: node({
+      mode: 'standalone'
+  }),
   vite: {
     ssr: {
       noExternal: ['@libsql/client']
